4721378da5041f3bc63a37f57ca2077f0226e7eb
coutvv <coutvv@gmail.com>
2015-08-08 18:52:15 +0300

    Console chat for twos

--- src/ru/coutvv/Client.java
+++ src/ru/coutvv/Client.java
@@ -5,35 +5,65 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+
 
 public class Client {
 	
 	Socket socket; 
+	
+	ObjectOutputStream output;
+	ObjectInputStream input;
+	
 	
 	public Client() throws UnknownHostException, IOException {
 		socket = new Socket("localhost", 4040);
 	}
 	
 	public void start() {
+		Resender bitch = null;
 		try {
 
-			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+			input = new ObjectInputStream(socket.getInputStream());
+			output = new ObjectOutputStream(socket.getOutputStream());
+			Scanner scan = new Scanner(System.in);
+			bitch = new Resender(); 
+			bitch.start();
+			System.out.println("Thats started");
 			while(true) {
-				String text = (String) input.readObject();
-				System.out.println(text);
+				String sendMsg = scan.nextLine();
+				output.writeObject(sendMsg);			
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
 		try {
-			new Client().start();
+			Client cl = new Client();
+			cl.start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+	}
+	private class Resender extends Thread {
+		private boolean stop = false;
+		public void stopIt() {
+			stop = true;
+		}
+		
+		public void run() {
+			while(!stop){
+				try {
+					String s = (String) input.readObject();
+					System.out.println(s);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}
 	}
 }
--- src/ru/coutvv/Server.java
+++ src/ru/coutvv/Server.java
@@ -1,8 +1,11 @@
 package ru.coutvv;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -15,22 +18,25 @@ import java.util.Scanner;
 public class Server {
 	ServerSocket server;
 	ObjectOutputStream output;
+	ObjectInputStream input;
 	
 	public Server() throws IOException {
 		server = new ServerSocket(4040);
 		
 	}
-	
-	public void addMessage(String msg) {
-		System.out.println(msg);
-	}
+
 	
 	public void start() {
+		Resender bitch = null;
 		try {
 			Socket friend = server.accept();
 			output = new ObjectOutputStream(friend.getOutputStream());
+			input = new ObjectInputStream(friend.getInputStream());
 			
 			Scanner scan = new Scanner(System.in);
+			bitch = new Resender(); 
+			bitch.start();
+			System.out.println("Thats started");
 			while(true) {
 				String sendMsg = scan.nextLine();
 				output.writeObject(sendMsg);			
@@ -39,8 +45,10 @@ public class Server {
 			e.printStackTrace();
 		} finally {
 			try {
+				bitch.stop();
 				server.close();
 				output.close();
+				input.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -57,6 +65,31 @@ public class Server {
 			it.start();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+
+	/**
+	 * Класс пишущий в консоль то, что получает с клиента
+	 * @author Jane Dow
+	 *
+	 */
+	private class Resender extends Thread {
+		private boolean stop = false;
+		public void stopIt() {
+			stop = true;
+		}
+		
+		public void run() {
+			while(!stop){
+				try {
+					String s = (String) input.readObject();
+					System.out.println(s);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
 		}
 	}
 }
