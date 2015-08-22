d73272895792670096e70bdde8d20175e21f1aeb
coutvv <coutvv@gmail.com>
2015-08-22 22:58:17 +0300

    подрефакторил

--- src/ru/coutvv/simplechat/server/Server.java
+++ src/ru/coutvv/simplechat/server/Server.java
@@ -25,7 +25,7 @@ public class Server {
 
 	
 	public void start() {
-		Resender msgWriter = null;
+		ClientConnection msgWriter = null;
 		try {
 			Broadcaster broadcaster = new Broadcaster();
 			broadcaster.start();
