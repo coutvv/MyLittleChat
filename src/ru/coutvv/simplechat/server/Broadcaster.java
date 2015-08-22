89be7307a630c3bec082ef4126c90703ced9045e
coutvv <coutvv@gmail.com>
2015-08-23 00:01:33 +0300

    Фиксил итераторную попаболь. Вроде успешно

--- src/ru/coutvv/simplechat/server/Broadcaster.java
+++ src/ru/coutvv/simplechat/server/Broadcaster.java
@@ -5,6 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import ru.coutvv.simplechat.data.Message;
@@ -16,23 +17,48 @@ public class Broadcaster extends Thread {
 	
 	
 	public void run() {
+		
 		while(true) {
-			for(ClientConnection client: clients) {
+
+			Iterator it = clients.iterator();
+			while(it.hasNext()) {
+				ClientConnection client = (ClientConnection) it.next();
+				if(client!= null && client.isNewMsg()){
+					Message msg = client.getMsg();
+					if(msg.getType() == Message.TYPE_LOGIN) {
+						System.out.println("устанавливаем имя клиенту");
+						client.setClientName(msg.getMsg());
+					} else if(msg.getType() == Message.TYPE_LOGOUT) {
+						client.stopIt();
+					}
+					System.out.println("ну тип отправляем всем посонам");
+					broadcast(msg, client);
+				}
 				if(client!=null && client.isStop()) {
 					System.out.println("Удаляем отключившегося клиента");
 					clients.remove(client);
 					break;
 				}
-				if(client!= null && client.isNewMsg()){
-					Message msg = client.getMsg();
-					if(msg.getType() == Message.TYPE_LOGIN) {
-						client.setClientName(msg.getMsg());
-					} else if(msg.getType() == Message.TYPE_LOGOUT) {
-						client.stopIt();
-					}
-					broadcast(msg, client);
-				}
 			}
+//			for(ClientConnection client: clients) {
+//				
+//				if(client!= null && client.isNewMsg()){
+//					Message msg = client.getMsg();
+//					if(msg.getType() == Message.TYPE_LOGIN) {
+//						System.out.println("устанавливаем имя клиенту");
+//						client.setClientName(msg.getMsg());
+//					} else if(msg.getType() == Message.TYPE_LOGOUT) {
+//						client.stopIt();
+//					}
+//					System.out.println("ну тип отправляем всем посонам");
+//					broadcast(msg, client);
+//				}
+//				if(client!=null && client.isStop()) {
+//					System.out.println("Удаляем отключившегося клиента");
+//					clients.remove(client);
+//					break;
+//				}
+//			}
 		}
 	}
 	
